0b5b95c5919daa8249fd20f320b50c4e05af2461
WaterSo0910 <jacky950141@gmail.com>
2022-05-04 20:48:28 +0800

    add some test

---
+++ apps/bowling/src/app/game.spec.ts
@@ -0,0 +1,33 @@
+import { Game } from './game';
+
+describe('Game', () => {
+  let game: Game;
+  beforeEach(() => {
+    game = new Game();
+  });
+
+  it('should create an instance', () => {
+    expect(game).toBeTruthy();
+  });
+  test('gutter game', () => {
+    rollMany(20, 0);
+
+    expect(game.score).toBe(0);
+  });
+  test('all one', () => {
+    rollMany(20, 1);
+    expect(game.score).toBe(20);
+  });
+  test('test on one spare', () => {
+    game.roll(5);
+    game.roll(5); // spare
+    game.roll(3);
+    rollMany(17, 0);
+    expect(game.score).toBe(16);
+  });
+  function rollMany(n: number, pins: number) {
+    for (let i = 0; i < n; i++) {
+      game.roll(pins);
+    }
+  }
+});
---
+++ apps/bowling/src/app/game.ts
@@ -0,0 +1,23 @@
+// game.ts
+export class Game {
+  private rolls: number[] = [];
+  private currentRoll = 0;
+
+  roll(pins: number) {
+    this.rolls[this.currentRoll++] = pins;
+  }
+
+  get score() {
+    let score = 0;
+    let i = 0;
+    for (let frame = 0; frame < 10; frame++) {
+      if (this.rolls[i] + this.rolls[i + 1] === 10) {
+        score += 10 + this.rolls[i + 2];
+      } else {
+        score += this.rolls[i] + this.rolls[i + 1];
+      }
+      i += 2;
+    }
+    return score;
+  }
+}
