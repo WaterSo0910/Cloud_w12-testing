c97cb739b7eeef2380f433c8b823c0d1137aafcd
WaterSo0910 <jacky950141@gmail.com>
2022-05-08 16:58:08 +0800

    create tests

---
+++ apps/potter/src/app/potter.ts
@@ -0,0 +1,17 @@
+import { PassThrough } from 'stream';
+
+// game.ts
+export class Potter {
+  private EUR_ONE_BOOK = 8;
+  private Discount = {
+    1: 0,
+    2: 0.05,
+    3: 0.1,
+    4: 0.2,
+    5: 0.25,
+  };
+
+  price(books: Array<number>) {
+    PassThrough;
+  }
+}
