caaa3bdc835c4d4cdd3fd86b1125d466df82907e
WaterSo0910 <jacky950141@gmail.com>
2022-05-08 19:57:22 +0800

    testEdgeCases pass

--- apps/potter/src/app/potter.ts
+++ apps/potter/src/app/potter.ts
@@ -1,10 +1,20 @@
 // game.ts
 class Counter<T> {
-  private list: Array<T> = [];
+  private _list: Array<T> = [];
   public dict: any = {};
   constructor(list: Array<T>) {
-    this.list = list;
-    list.forEach((element) => {
+    this._list = list;
+    this._list.forEach((element) => {
+      if (!this.dict[element]) {
+        this.dict[element] = 1;
+      } else {
+        this.dict[element] += 1;
+      }
+    });
+  }
+  set list(list: Array<T>) {
+    this._list = list;
+    this._list.forEach((element) => {
       if (!this.dict[element]) {
         this.dict[element] = 1;
       } else {
@@ -17,6 +27,13 @@ class Counter<T> {
   }
   get keys() {
     return Object.keys(this.dict) || [];
+  }
+  mostCommon() {
+    const arr = Object.keys(this.dict).map((obj) => [
+      Number(obj),
+      this.dict[obj],
+    ]);
+    return arr.sort((a, b) => b[1] - a[1]);
   }
 }
 export class Potter {
@@ -31,15 +48,35 @@ export class Potter {
   price(books: Array<number>) {
     let result = 0;
     while (books.length != 0) {
+      console.log(books);
+
       const booksCounter = new Counter<number>(books);
-      const uniNum = booksCounter.keys.length as number;
-      for (const k of booksCounter.keys.map(Number)) {
-        const index = books.indexOf(k, 0);
-        books.splice(index, 1);
+      let uniNum = booksCounter.keys.length as number;
+      if (books.length > 6 && books.length < 10 && uniNum === 5) {
+        let c = 0;
+
+        for (const jk of booksCounter.keys.map(Number)) {
+          if (c === 0) {
+            uniNum -= 1;
+            c += 1;
+            continue;
+          }
+          c += 1;
+          const k = books.indexOf(booksCounter.mostCommon()[0][0], 0);
+
+          books.splice(k, 1);
+          booksCounter.list = books;
+        }
+      } else {
+        for (const jk of booksCounter.keys.map(Number)) {
+          const k = books.indexOf(booksCounter.mostCommon()[0][0], 0);
+          books.splice(k, 1);
+          booksCounter.list = books;
+        }
       }
+      console.log(books);
       result += this.EUR_ONE_BOOK * (uniNum * (1 - this.Discount[uniNum]));
     }
-
     return result;
   }
 }
